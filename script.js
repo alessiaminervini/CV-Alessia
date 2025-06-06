5a2398da6ccd48be209eaa2018098174d5675deb
alessiaminervini <alessiaminervini254@gmail.com>
2025-06-06 13:45:38 +0200

    CV Alessia prel

---
+++ script.js
@@ -0,0 +1,10 @@
+let button1=document.querySelector("#progetti")
+let button2=document.querySelector("#contatti")
+
+
+button1.addEventListener("click", function(){
+    window.location.href="progettii.html"
+})
+button2.addEventListener("click", function(){
+    window.location.href="contatti.html"
+})
